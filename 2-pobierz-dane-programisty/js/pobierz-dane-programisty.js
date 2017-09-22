1dbf6623d7d07ce4996176efff8b9e97e1298f23
Krzysztof Stepniak <krzysztof.stepniak@softhouse.se>
2017-09-22 10:58:07 +0200

    dane  programisty - jeden div

--- 2-pobierz-dane-programisty/js/pobierz-dane-programisty.js
+++ 2-pobierz-dane-programisty/js/pobierz-dane-programisty.js
@@ -7,13 +7,9 @@ $(document).ready(function() {
     $("button").click(function(){
         $.getJSON("https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php", 
                   function(data){
-            $("body").append("<div id=dane-programisty>imie: "+ data.imie +"</div>");
+            $("body").append("<div id=dane-programisty>imie: "+ data.imie + "," + " nazwisko:" + data.nazwisko +"," + " zawod: " + data.zawod + "," + " firma: " + data.firma +"</div>");
             
-            $("body").append("<div id=dane-programisty>nazwisko: "+ data.nazwisko +"</div>");
-            
-            $("body").append("<div id=dane-programisty>zawod: "+ data.zawod +"</div>");
-            
-            $("body").append("<div id=dane-progarmisty>firma: "+ data.firma +"</div>");
+
             });
         });
     
